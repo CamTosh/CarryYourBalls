7ef23d75aa9433ee3db037f83006ffe9c1b74bb4
Tosh Camille <tochecamille@gmail.com>
2015-08-16 16:17:33 +0200

    Problème des "DM @[nom] [message]" fix + aurtograff

--- mp.php
+++ mp.php
@@ -5,9 +5,17 @@ $content =  $twitterConnection->get('account/verify_credentials');
 
 $getMP = $twitterConnection->get('direct_messages', array('count' => 1 ));
 
-	foreach ($getMP as $textMP) {
-	$roger = $textMP->text;
-	$htag = "#JPTC"; // Je porte tes couilles
-	 $twitterConnection->post('statuses/update', array('status' => $roger . " ". $htag));
-	}
+$toast = array(array('DM @'), array('D @'));
+ 
+if (in_array(array('DM @', 'D @'), $toast)) {
+	echo "none";
+} else {
+	foreach ($getMP as $TextMP) {
+	$roger = $TextMP->text;
+	$htag = "#JPTC";
+	$twitterConnection->post('statuses/update', array('status' => $roger . " ". $htag));
+	} 
+}
+ 
+
 ?>
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
 # Carry your balls
 
 Script utilisé pour https://twitter.com/CarryYourBalls
+
+Un pastebin pour expliquer : http://pastebin.com/FnhJBnyH
+
+
 De la merde, je le conçois, il fonctionne avec https://github.com/abraham/twitteroauth et il faut mettre ses tokens (app + user) dans le fichier 'key.php'.
 
 Le fichier refresh.py est là pour actualiser la page php du script, les tâches cron ne fonctionnaient pas sur mon serveur.
 - - -
-## Problémes
+## Problèmes
 Limitation de l'api Twitter. Les messages du type "DM @[nom] [messages]" qui spam une personne de DM.
+
+Attention aux conversations privées de groupe, ça spam beaucoup trop
